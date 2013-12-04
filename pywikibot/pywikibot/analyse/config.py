0f9d25fb958b5e5661db60a57eff5e6c42cebf20
raigo <raljand@gmail.com>
2013-12-04 09:30:01 +0200

    Logistic regression added

--- pywikibot/pywikibot/analyse/config.py
+++
@@ -1,9 +0,0 @@
-class Classification:
-    GOOD = 1
-    BAD = 2
-
-TRAINING_DATA_FILE = "clf.pkl"
-
-TRAIN_DATA = [(u"Reform", Classification.BAD),
-              (u"Füsiokraadid", Classification.GOOD),
-              (u"Oru vald (1939)", Classification.BAD)]
--- pywikibot/pywikibot/families/testviki_family.py
+++ pywikibot/pywikibot/families/testviki_family.py
@@ -5,8 +5,6 @@ from pywikibot.pywikibot import family, config2 as config
 import urllib
 import pywikibot.pywikibot.families.family
 # TestViki family
-# user_config.py:
-# usernames['mywiki']['et'] = 'user'
 class TestvikiFamily(family.Family):
    def __init__(self):
        family.Family.__init__(self)
